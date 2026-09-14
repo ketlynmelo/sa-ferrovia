a0bef98b2bf3fb4ca43ae8cdb3e68c5851b2c9ab
Slvrcoder <matheus_silveira9@estudante.sesisenai.org.br>
2026-09-14 09:09:45 -0300

    add - ajuste side bar e home

--- public/home_page/home.php
+++ public/home_page/home.php
@@ -7,7 +7,7 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <link rel="stylesheet" href="../style/style.css">
+    <link rel="stylesheet" href="../../style/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
 </head>
@@ -18,41 +18,41 @@
 
         <div class="text-center mb-5">
 
-            <img src="../assets/images/Simbolo.png" class="logo">
+            <img src="../../assets/images/Simbolo.png" class="logo">
 
         </div>
 
         <nav class="d-flex flex-column">
 
-            <a href="../public/home.html" class="menu-link active">
+            <a href="../../public/home.html" class="menu-link active">
 
                 <i class="bi bi-house-fill"></i>
                 <span>Início</span>
 
             </a>
 
-            <a href="../public/monitoramento.html" class="menu-link">
+            <a href="../../public/monitoramento.html" class="menu-link">
 
                 <i class="bi bi-graph-up"></i>
                 <span>Monitoramento</span>
 
             </a>
 
-            <a href="../public/relatorios.html" class="menu-link">
+            <a href="../../public/relatorios.html" class="menu-link">
 
                 <i class="bi bi-file-earmark-text"></i>
                 <span>Relatórios</span>
 
             </a>
 
-            <a href="../public/usuario.html" class="menu-link">
+            <a href="../../public/usuario.html" class="menu-link">
 
                 <i class="bi bi-people"></i>
                 <span>Usuários</span>
 
             </a>
 
-            <a href="../index.html" class="menu-link">
+            <a href="../../public/login.html" class="menu-link">
 
                 <i class="bi bi-box-arrow-right"></i>
                 <span>Sair</span>
@@ -76,7 +76,7 @@
                 <div class="user">
 
                     <span>
-                        <img src="../assets/images/usuário.png" class="img-fluid"> Administrador
+                        <img src="../../assets/images/usuário.png" class="img-fluid"> Administrador
                     </span>
 
                 </div>
@@ -94,7 +94,7 @@
 
                 </button>
 
-                <button href="../public/cadastrotrem.html" class="btn btn-custom" onclick="cadastrarTrem()">
+                <button href="../../public/cadastrotrem.html" class="btn btn-custom" onclick="cadastrarTrem()">
 
                     Cadastrar Trem
 
@@ -141,16 +141,16 @@
 
                                     <div class="action-buttons">
 
-                                        <button class="edit" href="../public/editsensor.html">
-                                            <img src="../assets/images/editbutton.png" id="editbutton">
+                                        <button class="edit" href="../../public/editsensor.html">
+                                            <img src="../../assets/images/editbutton.png" id="editbutton">
                                         </button>
 
-                                        <button class="delete" href="..public/deletesensor.html">
-                                            <img src="../assets/images/lixeirabutton.png" id="deletebutton">
+                                        <button class="delete" href="../../public/deletesensor.html">
+                                            <img src="../../assets/images/lixeirabutton.png" id="deletebutton">
                                         </button>
 
-                                        <button class="view" href="../public/monitoramento.html">
-                                            <img src="../assets/images/visualizabutton.png" id="viewbutton">
+                                        <button class="view" href="../../public/monitoramento.html">
+                                            <img src="../../assets/images/visualizabutton.png" id="viewbutton">
                                         </button>
 
                                     </div>
@@ -171,15 +171,15 @@
                                     <div class="action-buttons">
 
                                         <button class="edit">
-                                            <img src="../assets/images/editbutton.png" id="editbutton">
+                                            <img src="../../assets/images/editbutton.png" id="editbutton">
                                         </button>
 
                                         <button class="delete">
-                                            <img src="../assets/images/lixeirabutton.png" id="deletebutton">
+                                            <img src="../../assets/images/lixeirabutton.png" id="deletebutton">
                                         </button>
 
                                         <button class="view">
-                                            <img src="../assets/images/visualizabutton.png" id="viewbutton">
+                                            <img src="../../assets/images/visualizabutton.png" id="viewbutton">
                                         </button>
 
                                     </div>
@@ -200,15 +200,15 @@
                                     <div class="action-buttons">
 
                                         <button class="edit">
-                                            <img src="../assets/images/editbutton.png" id="editbutton">
+                                            <img src="../../assets/images/editbutton.png" id="editbutton">
                                         </button>
 
                                         <button class="delete">
-                                            <img src="../assets/images/lixeirabutton.png" id="deletebutton">
+                                            <img src="../../assets/images/lixeirabutton.png" id="deletebutton">
                                         </button>
 
                                         <button class="view">
-                                            <img src="../assets/images/visualizabutton.png" id="viewbutton">
+                                            <img src="../../assets/images/visualizabutton.png" id="viewbutton">
                                         </button>
 
                                     </div>
@@ -228,15 +228,15 @@
                                     <div class="action-buttons">
 
                                         <button class="edit">
-                                            <img src="../assets/images/editbutton.png" id="editbutton">
+                                            <img src="../../assets/images/editbutton.png" id="editbutton">
                                         </button>
 
                                         <button class="delete">
-                                            <img src="../assets/images/lixeirabutton.png" id="deletebutton">
+                                            <img src="../../assets/images/lixeirabutton.png" id="deletebutton">
                                         </button>
 
                                         <button class="view">
-                                            <img src="../assets/images/visualizabutton.png" id="viewbutton">
+                                            <img src="../../assets/images/visualizabutton.png" id="viewbutton">
                                         </button>
 
                                     </div>
@@ -257,15 +257,15 @@
                                     <div class="action-buttons">
 
                                         <button class="edit">
-                                            <img src="../assets/images/editbutton.png" id="editbutton">
+                                            <img src="../../assets/images/editbutton.png" id="editbutton">
                                         </button>
 
                                         <button class="delete">
-                                            <img src="../assets/images/lixeirabutton.png" id="deletebutton">
+                                            <img src="../../assets/images/lixeirabutton.png" id="deletebutton">
                                         </button>
 
                                         <button class="view">
-                                            <img src="../assets/images/visualizabutton.png" id="viewbutton">
+                                            <img src="../../assets/images/visualizabutton.png" id="viewbutton">
                                         </button>
 
                                     </div>
