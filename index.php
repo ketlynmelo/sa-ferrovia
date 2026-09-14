60e7d421bf2ff3aeaea7312c9fa9f4d168204592
ketlynmelo <ketlyn_de-melo@estudante.sesisenai.org.br>
2026-09-14 08:34:27 -0300

    add

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
 
 </head>
 <button onclick="location.href='public/usuario_page/usuario.php'">USUARIOS</button>
+<button onclick="location.href='public/monitoramento_page/monitoramento.php'">MONITORAMENTO</button>
+
 <body id=login>
     <div class="container">
         <div class="row justify-content-center">
