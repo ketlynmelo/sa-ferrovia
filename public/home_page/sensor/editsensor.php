b3392401edc0bef078d57a7ef7cf8e1264b4035f
IGOR-GAB30 <igor_paradella@estudante.sesisenai.org.br>
2026-09-14 21:51:10 -0300

    fix - arrumei a side bar e style das páginas cadastrosenson, editsensor, deletesensor e cadastrotrem.

--- public/home_page/sensor/editsensor.php
+++ public/home_page/sensor/editsensor.php
@@ -34,58 +34,45 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="../../style/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-
 </head>
-
+ 
 <body>
-
+ 
     <aside class="sidebar">
-
+ 
         <div class="text-center mb-5">
-
             <img src="../../assets/images/Simbolo.png" class="logo">
-
         </div>
-
+ 
         <nav class="d-flex flex-column">
-
-            <a href="../../public/home.html" class="menu-link active">
-
+ 
+            <a href="../home_page/home.php" class="menu-link active">
                 <i class="bi bi-house-fill"></i>
                 <span>Início</span>
-
             </a>
-
-            <a href="../../public/monitoramento.html" class="menu-link">
-
+ 
+            <a href="../monitoramento_page/monitoramento.php" class="menu-link">
                 <i class="bi bi-graph-up"></i>
                 <span>Monitoramento</span>
-
             </a>
-
-            <a href="../../public/relatorios.html" class="menu-link">
-
+ 
+            <a href="../relatorios_page/relatorios.php" class="menu-link">
                 <i class="bi bi-file-earmark-text"></i>
                 <span>Relatórios</span>
-
             </a>
-
-            <a href="../../public/usuario.html" class="menu-link">
-
+ 
+            <a href="../usuario_page/usuario.php" class="menu-link">
                 <i class="bi bi-people"></i>
                 <span>Usuários</span>
-
             </a>
-
-            <a href="../../public/login.html" class="menu-link">
-
+ 
+            <a href="../../index.php" class="menu-link">
                 <i class="bi bi-box-arrow-right"></i>
                 <span>Sair</span>
-
             </a>
-
+ 
         </nav>
-
+ 
     </aside>
 
     <main class="main">
--- public/home_page/trem/cadastrotrem.php
+++ public/home_page/trem/cadastrotrem.php
@@ -26,63 +26,50 @@ if (isset($_POST['cadastrar'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>XRail</title>
 
+   
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <link rel="stylesheet" href="../style/style.css">
+    <link rel="stylesheet" href="../../style/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-
 </head>
-
+ 
 <body>
-
-
+ 
     <aside class="sidebar">
-
+ 
         <div class="text-center mb-5">
-
-            <img src="../assets/images/Simbolo.png" class="logo">
-
+            <img src="../../assets/images/Simbolo.png" class="logo">
         </div>
-
+ 
         <nav class="d-flex flex-column">
-
-            <a href="public/home.html" class="menu-link active">
-
+ 
+            <a href="../home_page/home.php" class="menu-link active">
                 <i class="bi bi-house-fill"></i>
                 <span>Início</span>
-
             </a>
-
-            <a href="public/monitoramento.html" class="menu-link">
-
+ 
+            <a href="../monitoramento_page/monitoramento.php" class="menu-link">
                 <i class="bi bi-graph-up"></i>
                 <span>Monitoramento</span>
-
             </a>
-
-            <a href="public/relatorios.html" class="menu-link">
-
+ 
+            <a href="../relatorios_page/relatorios.php" class="menu-link">
                 <i class="bi bi-file-earmark-text"></i>
                 <span>Relatórios</span>
-
             </a>
-
-            <a href="public/usuario.html" class="menu-link">
-
+ 
+            <a href="../usuario_page/usuario.php" class="menu-link">
                 <i class="bi bi-people"></i>
                 <span>Usuários</span>
-
             </a>
-
-            <a href="public/login.html" class="menu-link">
-
+ 
+            <a href="../../index.php" class="menu-link">
                 <i class="bi bi-box-arrow-right"></i>
                 <span>Sair</span>
-
             </a>
-
+ 
         </nav>
-
+ 
     </aside>
 
     <main class="main">
